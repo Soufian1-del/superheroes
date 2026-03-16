67a7796bb3af4e00362e4c9ad8dfa00a0b83e97f
sourrxz <sourrxz@gmail.com>
2026-03-16 10:09:56 +0100

    finished the attributes in the superhero model

--- src/main/java/es/fplumara/dam1/superheroes/model/Superheroes.java
+++ src/main/java/es/fplumara/dam1/superheroes/model/Superheroes.java
@@ -1,4 +1,17 @@
 package es.fplumara.dam1.superheroes.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Superheroes {
+    private String nombre;
+    private String apodo;
+    private Franquicia franquicia;
+    private String poder;
 }
