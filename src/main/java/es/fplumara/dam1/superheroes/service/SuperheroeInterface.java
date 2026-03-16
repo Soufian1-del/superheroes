504308179620f83b8128ab81afb3ccc0af4caa63
sourrxz <sourrxz@gmail.com>
2026-03-16 10:57:37 +0100

    finished the service, added a validation method in model

--- src/main/java/es/fplumara/dam1/superheroes/service/SuperheroeInterface.java
+++ src/main/java/es/fplumara/dam1/superheroes/service/SuperheroeInterface.java
@@ -1,4 +1,16 @@
 package es.fplumara.dam1.superheroes.service;
 
+import es.fplumara.dam1.superheroes.model.Superheroes;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface SuperheroeInterface {
+    Optional<Superheroes> FindHero(String name);
+
+    List<Superheroes> FindHeroes();
+
+    Superheroes insertHero(Superheroes Superheroe);
+
+    Superheroes updateHero(Superheroes Superheroe);
 }
